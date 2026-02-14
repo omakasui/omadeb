ccb41729720378f2a4cad80d413cd7215f8182f1
Luca Pattocchio <pattocchioluca@gmail.com>
2026-02-14 22:34:38 +0100

    Use direct download

--- install/packaging/fonts.sh
+++ install/packaging/fonts.sh
@@ -8,5 +8,10 @@ unzip CascadiaMono.zip -d CascadiaFont
 cp CascadiaFont/*.ttf ~/.local/share/fonts
 rm -rf CascadiaMono.zip CascadiaFont
 
+wget https://assets.ubuntu.com/v1/0cef8205-ubuntu-font-family-0.83.zip -O fonts-ubuntu.zip
+unzip fonts-ubuntu.zip -d fonts-ubuntu
+cp fonts-ubuntu*.ttf ~/.local/share/fonts
+rm -rf fonts-ubuntu.zip fonts-ubuntu
+
 fc-cache
 cd -
