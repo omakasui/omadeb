d9452d5365b00faba216d212e896a9a9122502cd
Luca Pattocchio <pattocchioluca@gmail.com>
2026-02-25 17:55:48 +0100

    Add Omakasui APT repository

--- install/preflight/mirror.sh
+++ install/preflight/mirror.sh
@@ -35,3 +35,15 @@ EOF
     fi
   fi
 fi
+
+# Add Omakasui APT repository
+curl -fsSL https://packages.omakasui.org/omakasui.gpg.key \
+  | gpg --dearmor \
+  | sudo tee /usr/share/keyrings/omakasui-apt.gpg > /dev/null
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/omakasui-apt.gpg] \
+  https://packages.omakasui.org $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") main" \
+  | sudo tee /etc/apt/sources.list.d/omakasui.list
+
+# Refresh the APT cache
+sudo apt update
