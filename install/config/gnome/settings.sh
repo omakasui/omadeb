8a9ad5de73573ab688f7190c6bd880d020769277
Luca Pattocchio <pattocchioluca@gmail.com>
2026-03-10 17:57:54 +0100

    Remove hotkeys setting for Dash to Dock extension in settings.sh

--- install/config/gnome/settings.sh
+++ install/config/gnome/settings.sh
@@ -16,9 +16,6 @@ gsettings set org.gnome.settings-daemon.plugins.power ambient-enabled false
 gsettings set org.gnome.mutter dynamic-workspaces false
 gsettings set org.gnome.desktop.wm.preferences num-workspaces 6
 
-# Disable the hotkeys in the Dash to Dock extension (most likely culprit)
-gsettings set org.gnome.shell.extensions.dash-to-dock hot-keys false
-
 # Disable the night light feature by default
 gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled false
 # Set night light schedule to manual (so it doesn't turn on automatically)
