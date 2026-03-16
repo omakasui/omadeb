7bb52ac5201669c7c8859a58b21ede68cbec6aea
Luca Pattocchio <pattocchioluca@gmail.com>
2026-03-16 12:59:52 +0100

    Fix first-run scripts

--- install/preflight/first-run-mode.sh
+++ install/preflight/first-run-mode.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Set first-run mode marker so we can install stuff post-installation
 mkdir -p ~/.local/state/omadeb
 touch ~/.local/state/omadeb/first-run.mode
@@ -6,11 +8,13 @@ touch ~/.local/state/omadeb/first-run.mode
 mkdir -p ~/.config/systemd/user
 cat > ~/.config/systemd/user/omadeb-first-run.service <<EOF
 [Unit]
-Description=Omabuntu first-run setup
+Description=Omadeb first-run setup
 After=default.target
 
 [Service]
 Type=oneshot
+Environment=DISPLAY=:0
+Environment=DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/%U/bus
 ExecStart=/bin/bash $OMADEB_PATH/bin/omadeb-cmd-first-run
 ExecStartPost=systemctl --user disable omadeb-first-run.service
 RemainAfterExit=no
@@ -24,10 +28,12 @@ systemctl --user enable omadeb-first-run.service
 
 # Setup sudo-less access for first-run
 sudo tee /etc/sudoers.d/first-run >/dev/null <<EOF
-Cmnd_Alias FIRST_RUN_CLEANUP = /bin/rm -f /etc/sudoers.d/first-run
-$USER ALL=(ALL) NOPASSWD: /usr/bin/systemctl
-$USER ALL=(ALL) NOPASSWD: /usr/bin/ufw
-$USER ALL=(ALL) NOPASSWD: /usr/bin/ufw-docker
+Defaults:$USER !use_pty
+Cmnd_Alias FIRST_RUN_CLEANUP = /usr/bin/rm -f /etc/sudoers.d/first-run
+$USER ALL=(ALL) NOPASSWD: /usr/bin/systemctl *
+$USER ALL=(ALL) NOPASSWD: /usr/bin/ufw *
+$USER ALL=(ALL) NOPASSWD: /usr/sbin/ufw *
+$USER ALL=(ALL) NOPASSWD: /usr/local/bin/ufw-docker *
 $USER ALL=(ALL) NOPASSWD: FIRST_RUN_CLEANUP
 EOF
 sudo chmod 440 /etc/sudoers.d/first-run
