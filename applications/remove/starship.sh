b60a80ebfba2b15355042e1d7f1e8e1b0fbe696d
Luca Pattocchio <pattocchioluca@gmail.com>
2026-03-23 10:14:24 +0100

    Use starship package instead of building from sources

--- applications/remove/starship.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-rm -rf ~/.local/bin/starship
-rm -rf ~/.cache/starship
--- install/packaging/tools.sh
+++ install/packaging/tools.sh
@@ -8,6 +8,3 @@ source ~/.local/share/omadeb/applications/install/docker.sh
 
 # Install GitHub CLI
 source ~/.local/share/omadeb/applications/install/github-cli.sh
-
-# Install Starship
-source ~/.local/share/omadeb/applications/install/starship.sh
