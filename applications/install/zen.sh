b013645508dd5842ad3e74db3869b57e066c3912
Luca Pattocchio <15154851+Kasui92@users.noreply.github.com>
2026-04-10 11:39:11 +0200

    Use zen-browser package

--- applications/install/zen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /tmp
-curl -s https://updates.zen-browser.app/install.sh -o zen-install.sh 2>/dev/null
-chmod +x zen-install.sh 2>/dev/null
-bash zen-install.sh >/dev/null 2>&1
-rm -f zen-install.sh 2>/dev/null
-cd -
