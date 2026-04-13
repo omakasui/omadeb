41f5437df58420a7e7fa958aeee8f2e0d0d7d9d0
Luca Pattocchio <15154851+Kasui92@users.noreply.github.com>
2026-04-13 22:39:38 +0200

    Refactor asdcontrol installation and setup sudo-less controls

--- install/config/all.sh
+++ install/config/all.sh
@@ -13,14 +13,18 @@ run_logged $OMADEB_INSTALL/config/terminal.sh
 run_logged $OMADEB_INSTALL/config/localdb.sh
 run_logged $OMADEB_INSTALL/config/walker-elephant.sh
 run_logged $OMADEB_INSTALL/config/fast-shutdown.sh
+run_logged $OMADEB_INSTALL/config/sudoless-asdcontrol.sh
 run_logged $OMADEB_INSTALL/config/omadeb-ai-skill.sh
+
 run_logged $OMADEB_INSTALL/config/hardware/fix-fkeys.sh
 run_logged $OMADEB_INSTALL/config/hardware/fix-framework-text-scaling.sh
+
 run_logged $OMADEB_INSTALL/config/gnome/app-grid.sh
 run_logged $OMADEB_INSTALL/config/gnome/dock.sh
 source $OMADEB_INSTALL/config/gnome/request-confirm.sh
 run_logged $OMADEB_INSTALL/config/gnome/extensions.sh
 run_logged $OMADEB_INSTALL/config/gnome/hotkeys.sh
 run_logged $OMADEB_INSTALL/config/gnome/settings.sh
+
 run_logged $OMADEB_INSTALL/config/theme.sh
 run_logged $OMADEB_INSTALL/config/zellij.sh
